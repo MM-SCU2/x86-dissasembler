5ae1e30e47bea111fd2b0cc1cdd952ab4cd62a86
MMS <mscuderi@gmail.com>
2020-12-29 16:52:50 -0300

    more structure added

---
+++ build/CMakeFiles/disassm.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/disassm.dir/src/decoder.c.o"
+  "CMakeFiles/disassm.dir/src/dissasm.c.o"
+  "CMakeFiles/disassm.dir/src/opcode_map.c.o"
+  "disassm.pdb"
+  "disassm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/disassm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
