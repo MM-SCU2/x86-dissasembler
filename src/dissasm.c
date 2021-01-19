901adbf18350d9d6d35a1debb03a08836c96bc84
MMS <mscuderi@gmail.com>
2021-01-19 15:25:01 -0300

    changes in README file

--- src/dissasm.c
+++ src/dissasm.c
@@ -11,17 +11,6 @@
 
 	Supported compiled format: ELF files (Executable Linkeable Format)
 
-	Intentions:
-
-	The project was born out of the passion for system programming and compilers. The idea was to produce a simple disassembler
-	capable of turning machine code back into ASM. Originally it was not planned to give support to 64 bits instructions but due 
-	to them not being very different from 32 bits instructions, the support may be added on the future.
-	Another reason to do this is beacuse I wanted to provide an implementation as an example for such a thing 
-	that wasn't too complicated to understand or insanely large, 
-	as for example the "objdump" utility of GNU whose source code can be found but due to its size 
-	it turns out to be very complicated to use it as a guide.
-
-	I hope it may be usefull for those of you that are interested in these topics.
 
 ======================================================================================================
 ******************************************************************************************************
